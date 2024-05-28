c1d626b5618590da60fd9dacc09ab659e61e6140
Hemanshu <hemanshu.mahajan@gmail.com>
2024-05-28 21:31:56 +0530

    all required pages added

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+
+function AppLayout() {
+  return (
+    <div>
+      <Header />
+      <Sidebar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+export default AppLayout;
